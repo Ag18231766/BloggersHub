65d83263b3e1d3f4911c540c0bf5b20cb570562a
Ag18231766 <amartyagupta18231766@gmail.com>
2024-09-01 10:44:58 +0530

    changing ui

--- frontend/src/pages/PostCreation.tsx
+++ frontend/src/pages/PostCreation.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ChangeEvent, useState } from "react";
 import { Navbar } from "../components/Navbar";
 import { TagArray, useGetTags } from "../hooks/GetTags";
 
